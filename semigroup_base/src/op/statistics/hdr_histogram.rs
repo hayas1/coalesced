ee18032569b91b7d0161c49072525e4e8fcc5ba6
hayas1 <h4ystack@gmail.com>
2025-10-07 14:48:59 +0000

    HdrHistogram from iterator

--- semigroup_base/src/op/statistics/hdr_histogram.rs
+++ semigroup_base/src/op/statistics/hdr_histogram.rs
@@ -3,6 +3,8 @@ use semigroup_derive::ConstructionUse;
 
 use crate::{op::Construction, reverse::Reverse, semigroup::Semigroup};
 
+pub const DEFAULT_SIGFIG: u8 = 3;
+
 #[derive(Debug, Clone, PartialEq, ConstructionUse)]
 #[construction(op_trait = HdrHistogramExt)]
 pub struct HdrHistogram<T: Counter>(pub Histogram<T>);
@@ -15,7 +17,23 @@ impl<T: Counter> Semigroup for HdrHistogram<T> {
 #[cfg(feature = "monoid")]
 impl<T: Counter> crate::monoid::Monoid for HdrHistogram<T> {
     fn unit() -> Self {
-        Self(Histogram::new(3).unwrap_or_else(|_| unreachable!()))
+        Self(Histogram::new(DEFAULT_SIGFIG).unwrap_or_else(|_| unreachable!()))
+    }
+}
+impl<T: Counter> From<u64> for HdrHistogram<T> {
+    fn from(value: u64) -> Self {
+        let mut h = Histogram::new(DEFAULT_SIGFIG).unwrap_or_else(|_| unreachable!());
+        h += value;
+        Self(h)
+    }
+}
+impl<T: Counter> FromIterator<u64> for HdrHistogram<T> {
+    fn from_iter<I: IntoIterator<Item = u64>>(iter: I) -> Self {
+        let mut h = Histogram::new(DEFAULT_SIGFIG).unwrap_or_else(|_| unreachable!());
+        for v in iter {
+            h += v;
+        }
+        Self(h)
     }
 }
 
@@ -27,51 +45,40 @@ mod tests {
 
     #[test]
     fn test_hdr_histogram_as_semigroup_op() {
-        let mut h1 = Histogram::<u32>::new(3).unwrap();
-        h1 += 1;
-        let mut h2 = Histogram::new(3).unwrap();
-        h2 += 2;
-        let mut h3 = Histogram::new(3).unwrap();
-        h3 += 3;
-        let (a, b, c) = (HdrHistogram(h1), HdrHistogram(h2), HdrHistogram(h3));
+        let a: HdrHistogram<u32> = [1u64, 2, 3].into_iter().collect();
+        let b: HdrHistogram<u32> = [4, 5, 6].into_iter().collect();
+        let c: HdrHistogram<u32> = [7, 8, 9].into_iter().collect();
         assert_semigroup_op!(a, b, c);
     }
 
     #[test]
     fn test_hdr_histogram_as_monoid() {
-        let mut h1 = Histogram::<u32>::new(3).unwrap();
-        h1 += 1;
-        let mut h2 = Histogram::new(3).unwrap();
-        h2 += 2;
-        let mut h3 = Histogram::new(3).unwrap();
-        h3 += 3;
-        let (a, b, c) = (HdrHistogram(h1), HdrHistogram(h2), HdrHistogram(h3));
+        let a: HdrHistogram<u32> = [1u64, 2, 3].into_iter().collect();
+        let b: HdrHistogram<u32> = [4, 5, 6].into_iter().collect();
+        let c: HdrHistogram<u32> = [7, 8, 9].into_iter().collect();
         assert_monoid!(a, b, c);
     }
 
     #[test]
     fn test_hdr_histogram() {
-        let mut h1 = Histogram::<u32>::new(3).unwrap();
-        h1 += 1;
-        let mut h2 = Histogram::new(3).unwrap();
-        h2 += 2;
+        let a: HdrHistogram<u32> = [1u64, 2, 3].into_iter().collect();
+        let b: HdrHistogram<u32> = [4, 5, 6].into_iter().collect();
 
-        let (a, b) = (HdrHistogram(h1), HdrHistogram(h2));
         let res = a.clone().hdr_histogram(b.clone());
-        assert_eq!(res.max(), 2);
+        assert_eq!(res.max(), 6);
         assert_eq!(res.min(), 1);
-        assert_eq!(res.mean(), 1.5);
-        assert_eq!(res.len(), 2);
-        assert_eq!(res.value_at_quantile(0.5), 1);
-        assert_eq!(res.value_at_quantile(0.9), 2);
+        assert_eq!(res.mean(), 3.5);
+        assert_eq!(res.len(), 6);
+        assert_eq!(res.value_at_quantile(0.5), 3);
+        assert_eq!(res.value_at_quantile(0.9), 6);
 
         let (ra, rb) = (Reverse(a), Reverse(b));
         let Reverse(res) = ra.hdr_histogram(rb);
-        assert_eq!(res.max(), 2);
+        assert_eq!(res.max(), 6);
         assert_eq!(res.min(), 1);
-        assert_eq!(res.mean(), 1.5);
-        assert_eq!(res.len(), 2);
-        assert_eq!(res.value_at_quantile(0.5), 1);
-        assert_eq!(res.value_at_quantile(0.9), 2);
+        assert_eq!(res.mean(), 3.5);
+        assert_eq!(res.len(), 6);
+        assert_eq!(res.value_at_quantile(0.5), 3);
+        assert_eq!(res.value_at_quantile(0.9), 6);
     }
 }
