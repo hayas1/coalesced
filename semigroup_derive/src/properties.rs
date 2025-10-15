4d0e2979e704b5dd25cd3ab0b9005772b4042ea6
hayas1 <h4ystack@gmail.com>
2025-10-15 11:07:56 +0000

    snapshot

--- semigroup_derive/src/properties.rs
+++ semigroup_derive/src/properties.rs
@@ -21,62 +21,60 @@ pub fn impl_properties<C: ConstantExt>(
 
 #[cfg(test)]
 mod tests {
-    // use rstest::rstest;
+    use rstest::rstest;
 
-    // use crate::constant::{Absolute, Use};
+    use crate::constant::{Absolute, Use};
 
-    // use super::*;
+    use super::*;
 
-    // #[rstest]
-    // #[case::properties_annotated(
-    //     "properties_annotated",
-    //     impl_properties::<Absolute>,
-    //     syn::parse_quote! {
-    //         #[derive(Semigroup)]
-    //         #[semigroup(annotated)]
-    //         pub struct NamedStruct {
-    //             #[semigroup(with = "semigroup::op::annotation::overwrite::Overwrite")]
-    //             pub foo: String,
-    //             pub bar: Option<u32>,
-    //             pub baz: semigroup::op::annotation::overwrite::Overwrite<bool>,
-    //         }
-    //     },
-    // )]
-    // #[case::properties_not_annotated(
-    //     "properties_not_annotated",
-    //     impl_properties::<Use>,
-    //     syn::parse_quote! {
-    //         #[derive(SemigroupUse)]
-    //         #[semigroup(with = "semigroup::op::annotation::overwrite::Overwrite")]
-    //         pub struct UnnamedStruct<T: std::ops::Add> (
-    //             #[semigroup(with = "semigroup::op::semigroup::add::Added")]
-    //             T,
-    //             u64
-    //         );
-    //     },
-    // )]
-    // #[case::properties_custom_annotation(
-    //     "properties_custom_annotation",
-    //     impl_properties::<Absolute>,
-    //     syn::parse_quote! {
-    //         #[derive(Semigroup)]
-    //         #[semigroup(annotated, annotation_param = X, with = "semigroup::op::annotation::overwrite::Overwrite")]
-    //         pub struct NamedStruct{
-    //             pub foo: String,
-    //             pub bar: Option<u32>,
-    //             pub baz: bool,
-    //         }
-    //     },
-    // )]
-    // fn test_derive_properties_snapshot(
-    //     #[case] case: &str,
-    //     #[case] f: impl Fn(&ContainerAttr, &ItemStruct) -> syn::Result<TokenStream>,
-    //     #[case] (attr, input): (ContainerAttr, ItemStruct),
-    // ) {
-    //     let generated = f(&attr, &input).unwrap();
-    //     let formatted = prettyplease::unparse(&syn::parse2(generated).unwrap());
-    //     insta::with_settings!({ snapshot_path => "../tests/snapshots", prepend_module_to_snapshot => false }, {
-    //         insta::assert_snapshot!(case, formatted);
-    //     });
-    // }
+    #[rstest]
+    #[case::properties_annotated(
+        "properties_annotated",
+        impl_properties::<Absolute>,
+        (
+            syn::parse_quote! {
+                annotated, monoid
+            },
+            syn::parse_quote! {
+                /// A semigroup construction that returns the first non-`None` value.
+                /// # Properties
+                /// <!-- properties -->
+                #[derive(Construction)]
+                #[construction(annotated, monoid)]
+                pub struct Coalesce<T>(pub Option<T>);
+            },
+        ),
+    )]
+    #[case::properties_not_annotated(
+        "properties_not_annotated",
+        impl_properties::<Use>,
+        (
+            syn::parse_quote! {
+
+            },
+            syn::parse_quote! {
+                /// A semigroup struct that returns the sum and overwrite
+                /// # Properties
+                /// <!-- properties -->
+                #[derive(SemigroupUse)]
+                #[semigroup(with = "semigroup::op::annotation::overwrite::Overwrite")]
+                pub struct UnnamedStruct<T: std::ops::Add> (
+                    #[semigroup(with = "semigroup::op::semigroup::add::Added")]
+                    T,
+                    u64
+                );
+            },
+        )
+    )]
+    fn test_derive_properties_snapshot(
+        #[case] case: &str,
+        #[case] f: impl Fn(&ContainerAttr, &ItemStruct) -> syn::Result<TokenStream>,
+        #[case] (attr, input): (ContainerAttr, ItemStruct),
+    ) {
+        let generated = f(&attr, &input).unwrap();
+        let formatted = prettyplease::unparse(&syn::parse2(generated).unwrap());
+        insta::with_settings!({ snapshot_path => "../tests/snapshots", prepend_module_to_snapshot => false }, {
+            insta::assert_snapshot!(case, formatted);
+        });
+    }
 }
