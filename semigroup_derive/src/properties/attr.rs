cbb2a3e9c6924aaa1fc72610d9cca51d6170ba57
hayas1 <h4ystack@gmail.com>
2025-10-20 14:25:27 +0000

    link header

--- semigroup_derive/src/properties/attr.rs
+++ semigroup_derive/src/properties/attr.rs
@@ -1,7 +1,5 @@
 use darling::FromMeta;
 
-use crate::name::{var_name, Name};
-
 #[derive(Debug, Clone, PartialEq, FromMeta)]
 #[darling(derive_syn_parse, and_then = Self::validate)]
 pub struct ContainerAttr {
@@ -17,22 +15,6 @@ pub struct ContainerAttr {
 impl ContainerAttr {
     pub fn validate(self) -> darling::Result<Self> {
         Ok(self)
-    }
-    pub fn attributes(&self) -> [&str; 3] {
-        let Self {
-            annotated,
-            monoid,
-            commutative,
-        } = self;
-        [
-            var_name!(annotated),
-            var_name!(monoid),
-            var_name!(commutative),
-        ]
-        .map(|Name(name)| name)
-    }
-    pub fn properties(&self) -> [bool; 3] {
-        [self.is_annotated(), self.is_monoid(), self.is_commutative()]
     }
     pub fn is_annotated(&self) -> bool {
         self.annotated
